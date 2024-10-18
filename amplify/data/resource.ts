ba12a3f0d36b59c4f5ae4f5710ac09ce82cc5bcf
Adam Snyder <asnyder@latlogic.com>
2024-10-18 19:40:11 -0400

    record and payments model (fix)

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,22 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
+  Record: a
+    .model({
+      recordId: a.id().required(),
+      name: a.string().required(),
+      category: a.string(),
+      amount: a.float().required(),
+      isRepeat: a.boolean(),
+      postDate: a.date().required(),
+      paymentId: a.id().required(),
+      payment: a.belongsTo('Payment', 'paymentId'),
+    })
+    .identifier(['recordId'])
+    .secondaryIndexes((index) => [
+      index('recordId').sortKeys(['postDate']),
+    ])
+    .authorization((allow) => [allow.publicApiKey()]),
   Payment: a
     .model({
       paymentId: a.id().required(),
@@ -17,21 +33,6 @@ const schema = a.schema({
     .identifier(['paymentId'])
     .secondaryIndexes((index) => [
       index('paymentId').sortKeys(['nextDate']),
-    ])
-    .authorization((allow) => [allow.publicApiKey()]),
-  Record: a
-    .model({
-      recordId: a.id().required(),
-      name: a.string().required(),
-      category: a.string(),
-      amount: a.float().required(),
-      isRepeat: a.boolean(),
-      postDate: a.date().required(),
-      payment: a.belongsTo('Payment', 'paymentId'),
-    })
-    .identifier(['recordId'])
-    .secondaryIndexes((index) => [
-      index('recordId').sortKeys(['postDate']),
     ])
     .authorization((allow) => [allow.publicApiKey()]),
 });
