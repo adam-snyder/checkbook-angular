e882805cc1f4f6b522d3f9263e83c58cd29afa93
Adam Snyder <asnyder@latlogic.com>
2024-10-19 18:44:04 -0400

    update models and add API client

--- amplify/functions/dynamodb-trigger/handler.ts
+++ amplify/functions/dynamodb-trigger/handler.ts
@@ -1,9 +1,28 @@
-import type { DynamoDBStreamHandler } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import type { DynamoDBStreamHandler } from "aws-lambda";
+import { Schema } from '../../data/resource';
 
 const logger = new Logger({
   logLevel: "INFO",
   serviceName: "dynamodb-stream-handler",
+});
+
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: 'https://h5z6p4lk5fbjroqw4esr5ygwwa.appsync-api.us-east-1.amazonaws.com/graphql',
+        region: 'us-east-1',
+        defaultAuthMode: 'apiKey',
+      },
+    },
+  }
+);
+
+const client = generateClient<Schema>({
+  authMode: 'apiKey'
 });
 
 export const handler: DynamoDBStreamHandler = async (event) => {
@@ -18,7 +37,17 @@ export const handler: DynamoDBStreamHandler = async (event) => {
   }
   logger.info(`Successfully processed ${event.Records.length} records.`);
 
-  return {
-    batchItemFailures: [],
-  };
+  return await client.models.Payment.list()
+    .then((result) => {
+      console.log('result', result);
+
+      return {
+        batchItemFailures: [],
+      };
+    });
+
+  //
+  // return {
+  //   batchItemFailures: [],
+  // };
 };
