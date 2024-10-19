5c0b8bc758943b36890cbaec5450de080f190cfd
Adam Snyder <asnyder@latlogic.com>
2024-10-18 20:03:52 -0400

    remove todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,13 @@ import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/u
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { RecordsComponent } from './records/records.component';
-import { TodosComponent } from './todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent, RecordsComponent, AmplifyAuthenticatorModule],
+  imports: [RouterOutlet, RecordsComponent, AmplifyAuthenticatorModule],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
